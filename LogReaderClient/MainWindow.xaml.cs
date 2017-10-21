71500d2fae22908c58f175ec6b26676add0fbcda
Kristiansax <krellesax@hotmail.com>
2017-10-22 00:07:28 +0200

    Added all

---
+++ LogReaderClient/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LogReaderClient.Models;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.ObjectModel;
+
+namespace LogReaderClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Incident> CollectionforListView { get; set; }
+        static HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            RunAsync().Wait();
+            client.BaseAddress = new Uri("http://localhost:1361/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            InitializeComponent();
+        }
+        static async Task<List<Incident>> GetProductAsync(string path)
+        {
+            List<Incident> incident = new List<Incident>();
+            incident = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                incident = await response.Content.ReadAsAsync<List<Incident>>();
+            }
+            return incident;
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("http://localhost:1361/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        private async void fetchlog_Click(object sender, RoutedEventArgs e)
+        {
+            connectionErrorTextBlock.Visibility = Visibility.Hidden;
+
+            try
+            {
+                CollectionforListView = await GetProductAsync("api/log/getlog");
+                CollectionforListView.RemoveAt(0);
+
+                foreach (Incident incident in CollectionforListView)
+                {
+                    listView.Items.Add(incident);
+                }
+            }
+            catch (Exception)
+            {
+                connectionErrorTextBlock.Visibility = Visibility.Visible;
+            }
+
+        }
+    }
+}
---
+++ LogReaderClient/Models/Incident.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogReaderClient.Models
+{
+    public class Incident
+    {
+        public string time { get; set; }
+        public string ID { get; set; }
+        public string alarm { get; set; }
+        public string name { get; set; }
+        public string department { get; set; }
+        public string residence { get; set; }
+        public string location { get; set; }
+        public string resetTime { get; set; }
+        public string personnel { get; set; }
+        public string callTo { get; set; }
+        public string groupCall { get; set; }
+
+        public Incident(string time, string ID, string alarm, string name, string department, string residence, string location, string resetTime, string personnel, string callTo, string groupCall)
+        {
+            this.time = time;
+            this.ID = ID;
+            this.alarm = alarm;
+            this.name = name;
+            this.department = department;
+            this.residence = residence;
+            this.location = location;
+            this.resetTime = resetTime;
+            this.personnel = personnel;
+            this.callTo = callTo;
+            this.groupCall = groupCall;
+        }
+        public Incident()
+        {
+
+        }
+    }
+}
